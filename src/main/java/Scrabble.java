e9174947c671899f7b810d9015217064e8d3c42f
Cameron Jensen and Gloria Friesen <cameron@>
2017-03-15 10:28:31 -0700

    Add one letter will return a letterValue.

---
+++ src/main/java/Scrabble.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scrabble {
+  public Integer calculateScore(String letters) {
+    Integer letterValue = 0;
+    if (letters.equals("a")) {
+      letterValue = 1;
+      return letterValue;
+    }
+    return letterValue;
+  }
+}
