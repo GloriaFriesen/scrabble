1c75365c08be78eda191635f1ca1191f95229034
Cameron Jensen and Gloria Friesen <gloria@>
2017-03-15 11:06:49 -0700

    add one letter worth one point or two points

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,11 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class Scrabble {
   public Integer calculateScore(String letters) {
+    String[] onePointLetters  = {"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"};
+    List onePointList = Arrays.asList(onePointLetters);
+    String[] twoPointLetters = {"d", "g"};
+    List twoPointList = Arrays.asList(twoPointLetters);
     Integer letterValue = 0;
-    if (letters.equals("a")) {
+    if (onePointList.contains(letters)) {
       letterValue = 1;
+      return letterValue;
+    } else if (twoPointList.contains(letters)){
+      letterValue = 2;
       return letterValue;
     }
     return letterValue;
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -11,4 +11,28 @@ public class ScrabbleTest {
     Integer expected = 1;
     assertEquals(expected, testScrabble.calculateScore("a"));
   }
+
+  @Test
+  public void calculateScore_returnsScoreForOnePointLetters_1(){
+    Scrabble testScrabble = new Scrabble();
+    Integer expected = 1;
+    assertEquals(expected, testScrabble.calculateScore("a"));
+    assertEquals(expected, testScrabble.calculateScore("e"));
+    assertEquals(expected, testScrabble.calculateScore("i"));
+    assertEquals(expected, testScrabble.calculateScore("o"));
+    assertEquals(expected, testScrabble.calculateScore("u"));
+    assertEquals(expected, testScrabble.calculateScore("l"));
+    assertEquals(expected, testScrabble.calculateScore("n"));
+    assertEquals(expected, testScrabble.calculateScore("r"));
+    assertEquals(expected, testScrabble.calculateScore("s"));
+    assertEquals(expected, testScrabble.calculateScore("t"));
+  }
+
+  @Test
+  public void calculateScore_returnsScoreForTwoPointLetter_2(){
+    Scrabble testScrabble = new Scrabble();
+    Integer expected = 2;
+    assertEquals(expected, testScrabble.calculateScore("d"));
+    assertEquals(expected, testScrabble.calculateScore("g"));
+  }
 }
